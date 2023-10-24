762b872ecf40c2b27400708f3ba690aabec5a4bb
Apokalypt <58733915+Apokalypt@users.noreply.github.com>
2023-10-24 16:33:22 +0200

    test(OfficeQueue): Add tests on route + service for the ticket

---
+++ back-end/__tests__/office-queue.service.js
@@ -0,0 +1,41 @@
+require('jest');
+const db = require('../src/services/db');
+const service = require("../src/services/office-queue");
+const { UnknownServiceOfficeError } = require("../src/errors/UnknownServiceOfficeError");
+
+describe('[SERVICE] Check office queue service', () => {
+    const ticketIdsToDelete = [];
+
+    afterAll(() => {
+        for (const ticketId of ticketIdsToDelete) {
+            db.prepare('DELETE FROM tickets WHERE id = ?').run(ticketId);
+        }
+    });
+
+    describe('Add ticket to a service queue', () => {
+        let officeService;
+        beforeAll(() => {
+            officeService = db.prepare('SELECT * FROM services LIMIT 1').get();
+
+            expect(officeService).toHaveProperty('code');
+            expect(officeService).toHaveProperty('label');
+        });
+
+        test('should return a ticket', async () => {
+            const ticket = await service.addTicketToQueue(officeService.code);
+
+            expect(ticket).toHaveProperty('id');
+            ticketIdsToDelete.push(ticket.id);
+            expect(ticket).toHaveProperty('service_code');
+            expect(ticket).toHaveProperty('creation_date');
+            expect(ticket).toHaveProperty('counter_id');
+            expect(ticket).toHaveProperty('serving_date');
+            expect(ticket).toHaveProperty('completion_date');
+        });
+
+        test('should throw an error since service doesn\'t exist', async () => {
+            return expect(service.addTicketToQueue('unknown-service'))
+                .rejects.toThrow(UnknownServiceOfficeError);
+        });
+    });
+});
---
+++ back-end/__tests__/office-service.routes.js
@@ -0,0 +1,49 @@
+require('jest');
+const request = require("supertest");
+const app = require('../app');
+const service = require('../src/services/office-queue');
+const { UnknownServiceOfficeError } = require("../src/errors/UnknownServiceOfficeError");
+
+jest.mock('../src/services/office-queue', () => ({
+    addTicketToQueue: jest.fn(),
+}));
+
+describe('[ROUTES] Office services', () => {
+    describe('POST /api/services/:code/queue', () => {
+        test('should return the formatted ticket while request success', async () => {
+            service.addTicketToQueue.mockResolvedValue({
+                id: 1,
+                service_code: 'A01',
+                creation_date: '2021-01-01T00:00:00.000Z',
+                counter_id: null,
+                serving_date: null,
+                completion_date: null,
+            });
+
+            const res = await request(app).post('/api/services/A01/queue');
+
+            expect(res.statusCode).toEqual(201);
+            expect(res.body).toEqual({
+                id: 1,
+                serviceCode: 'A01',
+                creationDate: '2021-01-01T00:00:00.000Z',
+                counterId: null,
+                servingDate: null,
+                completionDate: null
+            });
+            expect(service.addTicketToQueue).toHaveBeenCalledWith('A01');
+        });
+
+        test('should return 404 error while service code is not found', async () => {
+            service.addTicketToQueue.mockRejectedValue(new UnknownServiceOfficeError('A02'));
+
+            const res = await request(app).post('/api/services/A02/queue');
+
+            expect(res.statusCode).toEqual(404);
+            expect(res.body).toEqual({
+                message: 'The service "A02" targeted by the request is unknown.',
+            });
+            expect(service.addTicketToQueue).toHaveBeenCalledWith('A02');
+        });
+    });
+});
