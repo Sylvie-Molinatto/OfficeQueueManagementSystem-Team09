02d7220e98e1edce0f1eb72ed00c181d05237a52
micheleschiavone00 <micheleschiavone00@gmail.com>
2023-10-25 17:27:49 +0200

    add officerLayout to call next customer

--- front-end/src/components/PageLayout.jsx
+++ front-end/src/components/PageLayout.jsx
@@ -1,5 +1,6 @@
 import { Form, Button, Container, Row, Col, Card, Dropdown, DropdownButton, Alert } from "react-bootstrap";
 import React, { useState } from 'react';
+let tickets = ["A10", "B20"];
 
 
 function ClientLayout() {
@@ -7,66 +8,97 @@ function ClientLayout() {
   const [ticketNumber2, setTicketNumber2] = useState(0);
   const [ticketNumber3, setTicketNumber3] = useState(0);
   const [newTicket, setNewTicket] = useState(null);
+  
+
 
   const centerStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
   };
 
   const generateTicket = (service) => {
-    let generatedTicket;
-    switch (service) {
-      case 1:
-        generatedTicket = `A${ticketNumber1}`;
-        setTicketNumber1(ticketNumber1 + 1);
-        break;
-      case 2:
-        generatedTicket = `B${ticketNumber2}`;
-        setTicketNumber2(ticketNumber2 + 1);
-        break;
-      case 3:
-        generatedTicket = `C${ticketNumber3}`;
-        setTicketNumber3(ticketNumber3 + 1);
-        break;
-      default:
-        generatedTicket = 'Invalid Service';
-    }
+      let generatedTicket;
+      switch (service) {
+          case 1:
+              generatedTicket = `A${ticketNumber1}`;
+              setTicketNumber1(ticketNumber1 + 1);
+              break;
+          case 2:
+              generatedTicket = `B${ticketNumber2}`;
+              setTicketNumber2(ticketNumber2 + 1);
+              break;
+          case 3:
+              generatedTicket = `C${ticketNumber3}`;
+              setTicketNumber3(ticketNumber3 + 1);
+              break;
+          default:
+              generatedTicket = 'Invalid Service';
+      }
 
-    // Aggiorna lo stato newTicket con il biglietto generato
-    setNewTicket(generatedTicket);
+      // Aggiorna lo stato newTicket con il biglietto generato
+      setNewTicket(generatedTicket);
+      tickets.push(generatedTicket);
   };
 
   return (
-    <Container style={centerStyle}>
-      <Row>
-        <Col>
-          <DropdownButton title="Get a Ticket">
-            <Dropdown.Item onClick={() => generateTicket(1)}>Servizio 1</Dropdown.Item>
-            <Dropdown.Item onClick={() => generateTicket(2)}>Servizio 2</Dropdown.Item>
-            <Dropdown.Item onClick={() => generateTicket(3)}>Servizio 3</Dropdown.Item>
-          </DropdownButton>
-        </Col>
-      </Row>
-      {newTicket && (
-        <Row>
-          <Col>
-            <Alert variant="success">
-              Your ticket is {newTicket}!
-            </Alert>
-          </Col>
-        </Row>
-      )}
-    </Container>
+      <Container style={centerStyle}>
+          <Row>
+              <Col>
+                  <DropdownButton title="Get a Ticket">
+                      <Dropdown.Item onClick={() => generateTicket(1)}>Servizio 1</Dropdown.Item>
+                      <Dropdown.Item onClick={() => generateTicket(2)}>Servizio 2</Dropdown.Item>
+                      <Dropdown.Item onClick={() => generateTicket(3)}>Servizio 3</Dropdown.Item>
+                  </DropdownButton>
+              </Col>
+          </Row>
+          {newTicket && (
+              <Row>
+                  <Col>
+                      <Alert variant="success">
+                          Your ticket is {newTicket}!
+                      </Alert>
+                  </Col>
+              </Row>
+          )}
+      </Container>
   );
 }
 
 function OfficerLayout(){
-  return(
-    <>
-    </>
-  );
+  const [newCustomer, setNewCustomer] = useState(null);
+    const centerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    };
+
+    const callCustomer = () => {
+        let calledCustomer= tickets.pop();
+        setNewCustomer(calledCustomer);
+    };
+
+
+    return (
+        <Container style={centerStyle}>
+            <Row>
+                <Col>
+                <Button variant="primary" onClick={() => callCustomer()}>Call Next Customer</Button>
+                </Col>
+            </Row>
+            {newCustomer && (
+                <Row>
+                    <Col>
+                        <Alert variant="success">
+                            Your next customer to be served is {newCustomer}!
+                        </Alert>
+                    </Col>
+                </Row>
+            )}
+        </Container>
+    );
 }
 
 function MonitorLayout(){
