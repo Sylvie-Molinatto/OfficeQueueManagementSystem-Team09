c08526945262579f79d4bb9c8b6a38c7ee113072
Daniele De Rossi <daniele.dr00@gmail.com>
2023-10-26 17:30:04 +0200

    Fix date in queries

--- back-end/src/services/counters.js
+++ back-end/src/services/counters.js
@@ -1,6 +1,7 @@
 const db = require("./db");
 const { UnknownCounterError } = require("../errors/UnknownCounterError");
 const { InvalidCounterStateError } = require("../errors/InvalidCounterStateError");
+const dayjs = require("dayjs");
 
 class CountersService {
     /**
@@ -37,7 +38,8 @@ class CountersService {
      * @return {Promise<Ticket>}
      */
     async indicateTicketAsServed(id) {
-        const res = db.prepare("UPDATE tickets SET completion_date = datetime('now') WHERE counter_id = ? AND serving_date IS NOT NULL AND completion_date IS NULL").run(id);
+        const date = dayjs().format("YYYY-MM-DD HH:mm:ss");
+        const res = db.prepare("UPDATE tickets SET completion_date = ? WHERE counter_id = ? AND serving_date IS NOT NULL AND completion_date IS NULL").run(id, date);
         if (res.changes !== 1) {
             // Invalid counter id or no ticket currently served, try to determine which one
             const counter = db.prepare("SELECT * FROM counters WHERE id = ?").get(id);
@@ -57,6 +59,7 @@ class CountersService {
      * @return {Promise<Ticket>}
      */
     async callCustomer(id) {
+        const date = dayjs().format("YYYY-MM-DD HH:mm:ss");
         // Self-called transaction
         db.transaction(() => {
             const counter = db.prepare("SELECT * FROM counters WHERE id = ?").get(id);
@@ -69,7 +72,7 @@ class CountersService {
                 throw new InvalidCounterStateError(`The counter with "${id}" is already serving a ticket and cannot call another customer.`)
             }
 
-            const res = db.prepare("UPDATE tickets SET counter_id = ?, serving_date = datetime() WHERE serving_date IS NULL AND completion_date IS NULL AND service_code IN (SELECT service_code FROM counters_services WHERE counter_id = ?)").run(id, id);
+            const res = db.prepare("UPDATE tickets SET counter_id = ?, serving_date = ? WHERE serving_date IS NULL AND completion_date IS NULL AND service_code IN (SELECT service_code FROM counters_services WHERE counter_id = ?)").run(id, date, id);
             if (res.changes !== 1) {
                 throw new InvalidCounterStateError(`No customer waiting for the counter with "${id}".`)
             }
